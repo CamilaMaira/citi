383bc7101c7807f3949469f3f77e211bd38a9602
CamilaMaira <camilamaira.m@gmail.com>
2020-03-12 14:10:08 -0300

    homepage y registrar ok

--- src/Components/PinPass.js
+++ src/Components/PinPass.js
@@ -1,66 +1,40 @@
-import React, {useState , useEffect}from 'react';
-import './PinPass.css'
-
-
-
-
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'
+import $ from "jquery";
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-
 const recognition = new SpeechRecognition();
-
 recognition.start();
-
-
 function PinPass() {
-
   const voiceCommands = () => {
-    recognition.onstart= () =>{
-      console.log("aca escuchan2")
-
+    // On start
+    recognition.onstart = () => {
+      console.log('Voice is actived');
     }
-  recognition.onresult = (e) =>{
-    let current = e.resultIndex;
-    let transcript = e.results[current][0].transcript;
-    console.log(transcript);
-  // Línea para arreglar bug en mobile
-    let mobileRepeatBug = (current === 1 && transcript === e.results[0][0].transcript);
-
-    if(!mobileRepeatBug) {
-      if(transcript.length === 4) {
-        document.getElementById('final').innerHTML = transcript
+    // Do something when we get a result
+    recognition.onresult = (e) => {
+      let current = e.resultIndex;
+      let transcript = e.results[current][0].transcript;
+      let mobileRepeatBug = (current === 1 && transcript === e.results[0][0].transcript);
+      if(!mobileRepeatBug) {
+        if(transcript  ) {
+          console.log(transcript);
+          document.getElementById("pin").innerHTML= transcript
+          setTimeout(() => {
+            $("#ok").click()
+          }, 3000); 
+        }
       }
-    }}}
-//     setTimeout(() => {
-//       recognition.start();
-//     }, 50);
-//   }
-//   recognition.onspeechend = () => {
-//     recognition.stop();
-//     console.log('voice stopped');
-//   }
-// }
-
-  useEffect(( ) =>{
+    }}
+  useEffect(() => {
     voiceCommands();
   });
-  // const ButtonsPin = () => {
-  //   let numbersbuttons = 9
-  //   for ( let i = 0 ; i <= numbersbuttons ; i++){
-  //  document.getElementById("numbers").innerHTML +=`<button class="numbersButtons">${i}</button> `
-  //  }}
-     
-  //    useEffect(( ) =>{
-  //      ButtonsPin();
-  //    });
-
   return (
-    <div id="containerPin" className="container-fluid row justify-content-center ">
-      <h3 className=" text-center mb-4">Hola Rodrigo</h3>
-      <div className="warning row justify-content-center text-center"><p>Introduce el PIN (número secreto) de tu tarjeta o usuario de Banca electrónica</p></div>
-     
-        <p id="final"></p>
-        <div className="acces text-center"> <p>NO PUEDO ACCEDER</p></div>
-        <div id="numbers" className="column justify-content-center">
+    <div className="">
+       <h3 className="">Hola Rodrigo</h3>
+       <div className=""><p>Introduce el PIN (número secreto) de tu tarjeta o usuario de Banca electrónica</p></div>
+      <p id="pin"></p>
+      <div className=""> <p>NO PUEDO ACCEDER</p></div>
+      <div id="numbers" className="">
           <div className="cont1">
             <button>1</button>
           <button>2</button>
@@ -80,12 +54,8 @@ function PinPass() {
           <button>0</button>
           <button>B</button></div>
         </div>
-  <div className="row justify-content-center"> <button type="button"   className="btn btn-light">Confirmar</button></div>
-</div>
+     <Link to="/hola"><button id="ok">Confirmar</button></Link>
+    </div>
   );
-};
-
+}
 export default PinPass;
-
-// estilos css
-
